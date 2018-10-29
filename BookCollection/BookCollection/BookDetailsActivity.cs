64b5c9ad77f0d76cbd03397109a76f623d6c7638
Tiffany Ford <rhysma@gmail.com>
2018-10-29 08:56:43 -0500

    Adding the book collection project completed on stream for reference.

---
+++ BookCollection/BookCollection/BookDetailsActivity.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System.Net;
+using System.IO;
+using Newtonsoft.Json;
+using SQLite;
+
+namespace BookCollection
+{
+    [Activity(Label = "BookDetailsActivity")]
+    public class BookDetailsActivity : Activity
+    {
+         string WEBSERVICE_URL = "https://www.googleapis.com/books/v1/volumes?q=isbn:";
+        string dbpath = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal);
+        Book currentBook;
+        string bookISBN;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            //setup a new blank database if one doesn't already exist
+            var db = new SQLiteConnection(System.IO.Path.Combine(dbpath, "BookCollection.db"));
+
+            db.CreateTable<IndustryIdentifier>();
+            //db.CreateTable<ReadingModes>();
+            //db.CreateTable<ImageLinks>();
+            //db.CreateTable<SaleInfo>();
+            //db.CreateTable<Epub>();
+            //db.CreateTable<Pdf>();
+            //db.CreateTable<SearchInfo>();
+            //db.CreateTable<AccessInfo>();
+            //db.CreateTable<VolumeInfo>();
+            //db.CreateTable<Item>();
+            //db.CreateTable<Book>();
+
+
+            // Create your application here
+            // Set our view from the layout resource
+            SetContentView(Resource.Layout.bookDetails);
+
+            //get the textview control
+            TextView details = FindViewById<TextView>(Resource.Id.details);
+
+            //get the content from the activity that is passed in
+            bookISBN = Intent.GetStringExtra("bookDetails");
+
+            try
+            {
+                WEBSERVICE_URL = WEBSERVICE_URL + bookISBN;
+
+                var webRequest = WebRequest.Create(WEBSERVICE_URL);
+
+                if (webRequest != null)
+                {
+                    webRequest.Method = "GET";
+                    webRequest.ContentType = "application/json";
+
+                    //Get the response 
+                    WebResponse wr = webRequest.GetResponseAsync().Result;
+                    Stream receiveStream = wr.GetResponseStream();
+                    StreamReader reader = new StreamReader(receiveStream);
+
+                    currentBook = JsonConvert.DeserializeObject<Book>(reader.ReadToEnd());
+
+                    foreach(var item in currentBook.items)
+                    {
+                        details.Text = "Title: " + item.volumeInfo.title + "\n";
+                        foreach(var author in item.volumeInfo.authors)
+                        {
+                            details.Text += "Author: " + author + "\n";
+                        }
+                        details.Text += "Published Date: " + item.volumeInfo.publishedDate + "\n";
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+
+            try
+            {
+                //check to see if this book is already available in the database
+                var hasBook = db.Query<IndustryIdentifier>("select * from IndustryIdentifier where identifier=?", bookISBN);
+                TextView responseText = FindViewById<TextView>(Resource.Id.bookResponse);
+
+                if (hasBook.Count != 0 )
+                {
+                    //a book has been found
+                    responseText.SetTextColor(Android.Graphics.Color.Green);
+                    responseText.Text = "You already own this book";
+                }
+                else
+                {
+                    //a book has not been found
+                    //if it isn't we're going to ask to add it
+                    responseText.SetTextColor(Android.Graphics.Color.Red);
+                    responseText.Text = "You do not own this book";
+                    Button addButton = FindViewById<Button>(Resource.Id.addButton);
+                    addButton.Click += AddButton_Click;
+                    addButton.Visibility = ViewStates.Visible;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+
+        }
+
+        private void AddButton_Click(object sender, EventArgs e)
+        {
+            //add the book into the collection
+            //setup a new blank database if one doesn't already exist
+            try
+            {
+                SQLiteConnection db = new SQLiteConnection(System.IO.Path.Combine(dbpath, "BookCollection.db"));
+                var command = db.CreateCommand("Insert into IndustryIdentifier (type, identifier) values (?, ?)", "ISBN", bookISBN);
+                command.ExecuteNonQuery();
+                
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            
+        }
+
+    }
+}
---
+++ BookCollection/BookCollection/obj/Debug/android/src/md500efa269f7ec69de34d1aad31db00e0b/BookDetailsActivity.java
@@ -0,0 +1,47 @@
+package md500efa269f7ec69de34d1aad31db00e0b;
+
+
+public class BookDetailsActivity
+	extends android.app.Activity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
+			"";
+		mono.android.Runtime.register ("BookCollection.BookDetailsActivity, BookCollection", BookDetailsActivity.class, __md_methods);
+	}
+
+
+	public BookDetailsActivity ()
+	{
+		super ();
+		if (getClass () == BookDetailsActivity.class)
+			mono.android.TypeManager.Activate ("BookCollection.BookDetailsActivity, BookCollection", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public void onCreate (android.os.Bundle p0)
+	{
+		n_onCreate (p0);
+	}
+
+	private native void n_onCreate (android.os.Bundle p0);
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
