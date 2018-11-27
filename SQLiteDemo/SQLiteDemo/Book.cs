6ffa817c20c9b3ca8b46caca2078fccd8b7fb080
Tiffany Ford <rhysma@gmail.com>
2018-11-27 11:19:05 -0600

    sqlite demo

---
+++ SQLiteDemo/SQLiteDemo/Book.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using SQLite;
+
+namespace SQLiteDemo
+{
+    [Table("Items")]
+    public class Book
+    {
+        [PrimaryKey, AutoIncrement, Column("_id")]
+        public int Id { get; set; }
+
+        public Book()
+        {
+
+        }
+
+        public Book(string title, string author, string isbn)
+        {
+            Title = title;
+            Author = author;
+            ISBN = isbn;
+        }
+
+        public string Title { get; set; }
+
+        public string Author { get; set; }
+
+        public string ISBN { get; set; }
+
+        public override string ToString()
+        {
+            return ISBN + "  " + Title + " by: " + Author;
+        }
+    }
+}
---
+++ SQLiteDemo/SQLiteDemo/ViewBooksActivity.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using SQLite;
+using System.Collections;
+
+namespace SQLiteDemo
+{
+    [Activity(Label = "ViewBooksActivity")]
+    public class ViewBooksActivity : ListActivity
+    {
+        List<Book> myBooks;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            SetContentView(Resource.Layout.ViewBooks);
+
+            //setup the list to hold the books
+            myBooks = new List<Book>();
+
+            //setup the path of where the db file will be stored
+            string dbpath = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal);
+
+            //setup a new blank database if one doesn't already exist
+            var db = new SQLiteConnection(System.IO.Path.Combine(dbpath, "books.db"));
+
+            //create a new table Book if it doesn't already exist
+            db.CreateTable<Book>();
+
+            //read each book that is in the table
+            var table = db.Table<Book>();
+            foreach(var b in table)
+            {
+                myBooks.Add(b);
+            }
+
+            ListAdapter = new ArrayAdapter<Book>(this, Android.Resource.Layout.SimpleListItem1, myBooks);
+        }
+    }
+}
